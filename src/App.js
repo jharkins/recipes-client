e5a5c0c87daa6af47e5511fbe257f2e773d49132
Joe Harkins <jharkins@gmail.com>
2024-05-25 12:14:15 -0600

    Refactor for Navbar

--- src/App.js
+++ src/App.js
@@ -1,41 +1,27 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Container } from "react-bootstrap";
 
 import Navigation from "./components/Navigation";
 
-import "./App.css";
 import Random from "./components/Random";
 import Recipe from "./components/Recipe";
 import Browse from "./components/Browse";
 import Search from "./components/Search";
+import "./App.css";
 
 const Layout = () => (
-  <Container fluid>
-    <Row>
-      <Col>
-        <h1>Loyning Cookbook</h1>
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={2}>
-        <Navigation />
-      </Col>
-      <Col>
-        <Container className="mt-2">
-          <Routes>
-            <Route path="/" element={<Browse />} />
-            <Route path="/random" element={<Random />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/recipe/:id" exact element={<Recipe />} />
-          </Routes>
-        </Container>
-      </Col>
-    </Row>
-  </Container>
+  <>
+    <Navigation />
+    <Container className="mt-4">
+      <Routes>
+        <Route path="/" element={<Browse />} />
+        <Route path="/random" element={<Random />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/recipe/:id" exact element={<Recipe />} />
+      </Routes>
+    </Container>
+  </>
 );
 
 const App = () => (
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 
+import { Container } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import { LinkContainer } from "react-router-bootstrap";
 
 const Navigation = () => (
-  <Nav className="flex-column" variant="pills">
-    <LinkContainer to="/">
-      <Nav.Link>Browse</Nav.Link>
-    </LinkContainer>
-    <LinkContainer to="/random">
-      <Nav.Link>Random</Nav.Link>
-    </LinkContainer>
-    <LinkContainer to="/search">
-      <Nav.Link>Search</Nav.Link>
-    </LinkContainer>
-  </Nav>
+  <Navbar bg="light" expand="lg">
+    <Container>
+      <Navbar.Brand href="/">Loyning Cookbook</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <LinkContainer to="/">
+            <Nav.Link>Browse</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/random">
+            <Nav.Link>Random</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/search">
+            <Nav.Link>Search</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
 );
 
 export default Navigation;
